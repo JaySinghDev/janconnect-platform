dac1e749905865fd434244f068959fea46a7fe99
Jay Singh <jay817070@gmail.com>
2026-06-06 22:16:55 +0530

    Initial commit: JanConnect Platform

---
+++ JanConnect/src/main/java/com/janconnect/JanConnectApplication.java
@@ -0,0 +1,13 @@
+package com.janconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JanConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JanConnectApplication.class, args);
+	}
+
+}
---
+++ JanConnect/src/test/java/com/janconnect/JanConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.janconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JanConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
